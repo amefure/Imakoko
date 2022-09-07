e7ee503428bf9988d77926ca9dda6a9df9850e4a
ame <shachi10969@gmail.com>
2022-09-07 19:07:54 +0900

    Initial Commit

---
+++ Imakoko/ImakokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImakokoApp.swift
+//  Imakoko
+//
+//  Created by t&a on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct ImakokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
